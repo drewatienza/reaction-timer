16d4e54f87c3f68b2ce80e10635679ad1ae8960e
drewatienza <atienza.drew@gmail.com>
2020-12-22 09:37:56 -0800

    implemented showing results component and added finishing touches to project

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,63 @@
 <template>
   <h1>Ninja Reaction Timer</h1>
-  <button @click="start" :disabled='isPlaying'>play</button>
-  <Block v-if="isPlaying" :delay="delay" @end='endGame' />
-  <p v-if="showResults">Reaction Time: {{ score }} ms</p>
+  <button @click="start" :disabled="isPlaying">play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <Results v-if="showResults" :score="score" />
 </template>
 
 <script>
-import Block from './components/Block'
+  import Block from "./components/Block";
+  import Results from "./components/Results";
 
-export default {
-  name: 'App',
-  components: { Block },
-  data() {
-    return {
-      isPlaying: false,
-      delay: null,
-      score: null,
-      showResults: false
-    }
-  },
-  methods: {
-    start() {
-      this.delay = 1000 + Math.random() * 6000
-      this.isPlaying = true
-      this.showResults = false
+  export default {
+    name: "App",
+    components: { Block, Results },
+    data() {
+      return {
+        isPlaying: false,
+        delay: null,
+        score: null,
+        showResults: false,
+      };
     },
-    endGame(reactionTime) {
-      this.score = reactionTime
-      this.isPlaying = false
-      this.showResults = true
-    }
-  }
-}
+    methods: {
+      start() {
+        this.delay = 1000 + Math.random() * 6000;
+        this.isPlaying = true;
+        this.showResults = false;
+      },
+      endGame(reactionTime) {
+        this.score = reactionTime;
+        this.isPlaying = false;
+        this.showResults = true;
+      },
+    },
+  };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #444;
-  background: #cfcfcf;
-  padding-top: 60px;
-  height: 100vh;
-}
-button {
-  text-transform: uppercase;
-  background: #444;
-  color: #fff;
-  border: 1px solid #fff;
-  height: 35px;
-  width: 100px;
-  font-size: 1.3rem;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #444;
+    background: #cfcfcf;
+    padding-top: 60px;
+    height: 100vh;
+  }
+  button {
+    text-transform: uppercase;
+    background: #444;
+    color: #fff;
+    border: 1px solid #fff;
+    height: 35px;
+    width: 100px;
+    font-size: 1.3rem;
+  }
+  button[disabled] {
+    opacity: 0.7;
+    background: #999;
+    cursor: not-allowed;
+  }
 </style>
---
+++ src/components/Results.vue
@@ -0,0 +1,34 @@
+<template>
+  <p>Reaction Time - {{ score }} ms</p>
+  <p class="rank">Rank: {{ rank }}</p>
+</template>
+
+<script>
+  export default {
+    props: ["score"],
+    data() {
+      return {
+        rank: null,
+      };
+    },
+    mounted() {
+      if (this.score < 300) {
+        this.rank = "Ninja";
+      } else if (this.score < 400) {
+        this.rank = "Cheetah";
+      } else if (this.score < 450) {
+        this.rank = "Gazelle";
+      } else {
+        this.rank = "Snail";
+      }
+    },
+  };
+</script>
+
+<style>
+  .rank {
+    font-size: 1.4em;
+    color: #822;
+    font-weight: bold;
+  }
+</style>
